1f6f28812ad3498950a3329fc4e2a3083160d882
TestUnit <testunit@mail.ru>
2018-12-22 02:19:33 +0700

    Fix Player not found при попытке поднять упавшего персонажа опцией помочь. Плавающий баг.
    https://trello.com/c/trjp5bgN/86-player-not-found-%D0%BF%D1%80%D0%B8-%D0%BF%D0%BE%D0%BF%D1%8B%D1%82%D0%BA%D0%B5-%D0%BF%D0%BE%D0%B4%D0%BD%D1%8F%D1%82%D1%8C-%D1%83%D0%BF%D0%B0%D0%B2%D1%88%D0%B5%D0%B3%D0%BE-%D0%BF%D0%B5%D1%80%D1%81%D0%BE%D0%BD%D0%B0%D0%B6%D0%B0-%D0%BE%D0%BF%D1%86%D0%B8%D0%B5%D0%B9-%D0%BF%D0%BE%D0%BC%D0%BE%D1%87%D1%8C-%D0%BF%D0%BB%D0%B0%D0%B2%D0%B0%D1%8E%D1%89%D0%B8%D0%B9-%D0%B1%D0%B0%D0%B3

--- Init.lua
+++ Init.lua
@@ -85,8 +85,8 @@ function RPSCoreFramework:OnInitialize()
 	DarkmoonCharStatsInfoSubmit:SetScript("OnClick", function() StaticPopup_Show("LearnStats") end);	
 	DarkmoonCharStatsInfoUnlearn:SetScript("OnClick", function() StaticPopup_Show("UnlearnStats") end);
 	
-	RPS_InteractFrameHelp:SetScript("OnClick", function() RPSCoreFramework:SendCoreMessage(".rps action help %t") end);
-	RPS_InteractFrameKill:SetScript("OnClick", function() RPSCoreFramework:SendCoreMessage(".rps action kill %t") end);
+	RPS_InteractFrameHelp:SetScript("OnClick", function()	local msg = ".rps action help "..GetUnitName("target")	RPSCoreFramework:SendCoreMessage(msg) end);
+	RPS_InteractFrameKill:SetScript("OnClick", function()	local msg = ".rps action kill "..GetUnitName("target")	RPSCoreFramework:SendCoreMessage(msg) end);
 	
 	RPSCoreFramework:StatsIncDecFunc()
 	
