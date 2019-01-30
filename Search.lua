9b562756b838b3819e99bcf4f693e1d2173638af
TestUnit <testunit@mail.ru>
2019-01-30 21:30:28 +0700

    Подводка поиска под единый strlower

--- Search.lua
+++ Search.lua
@@ -8,9 +8,9 @@ end
 function RPSCoreFramework:AurasSearch(input, key)
 	RPSCoreFramework.Interface.Auras.Show = {}
 	for i=1, #input do
-		if string.find(input[i][1],key) ~= nil then
+		if string.find(strlower(input[i][1]),strlower(key)) ~= nil then
 			table.insert(RPSCoreFramework.Interface.Auras.Show, i);
-		elseif string.find(input[i][2],key) ~= nil then
+		elseif string.find(strlower(input[i][2]),strlower(key)) ~= nil then
 			table.insert(RPSCoreFramework.Interface.Auras.Show, i);
 		end
 	end
