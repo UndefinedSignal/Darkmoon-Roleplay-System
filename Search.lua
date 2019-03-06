83e34f60b087b120e429b0f2391652ea3ee03fc5
TestUnit <testunit@mail.ru>
2019-03-07 00:21:51 +0700

    Auras search via number

--- Search.lua
+++ Search.lua
@@ -12,6 +12,8 @@ function RPSCoreFramework:AurasSearch(input, key)
 			table.insert(RPSCoreFramework.Interface.Auras.Show, i);
 		elseif string.find(strlower(input[i][3]),strlower(key)) ~= nil then
 			table.insert(RPSCoreFramework.Interface.Auras.Show, i);
+		elseif string.find(strlower(input[i][1]), strlower(key)) ~= nil then
+			table.insert(RPSCoreFramework.Interface.Auras.Show, i);
 		end
 	end
 end
